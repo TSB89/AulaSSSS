7bdfc5b55b95b1272e230395ac3e788e09e83495
Aluno <aluno@cesed.local>
2024-10-17 11:16:07 -0300

    saudacao

---
+++ Saudacao.java
@@ -0,0 +1,16 @@
+package treino;
+
+import java.util.Scanner;
+
+public class Saudacao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Olá, " + nome + "! Bem-vindo ao programa.");
+        
+        scanner.close();
+    }
+}
